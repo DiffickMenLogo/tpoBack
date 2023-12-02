c18f2ab8aa04e557d67139bd2cd7c27ba2a7bf7f
DiffickMenLogo <gh.sdfs@mail.ru>
2023-12-02 04:42:19 +0300

    feat: make resurses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import configuration from './config/configuration'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ProductsModule } from './products/products.module'
+import { ContactsModule } from './contacts/contacts.module'
+import { OrdersModule } from './orders/orders.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    ProductsModule,
+    ContactsModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
